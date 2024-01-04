f9380245f95b9a5f5bf84eaabd9d8e43e8dd97b7
Carlos Henrique <cacarmg@gmail.com>
2024-01-03 22:03:18 -0300

    Exercicios 2 e 4

---
+++ Exercicio_net_funcoes/MaiusculaMinuscula.js
@@ -0,0 +1,10 @@
+function maimin(a){
+    const res = a.toUpperCase()
+    return res;
+
+}
+
+console.log(`A palavra arame em letra maiuscula fica ${maimin("arame")}`)
+console.log(`A palavra aprender em letra maiuscula fica ${maimin("aprender")}`)
+console.log(`A frase hello_world em letra maiuscula fica ${maimin("hello_world")}`)
+
